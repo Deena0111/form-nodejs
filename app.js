feaaaecdb84b2b590562ee4ee7c6a6165fc85af6
Deena0111 <deena011197@gmail.com>
2023-04-17 19:32:20 +0530

    user info post method created

--- app.js
+++ app.js
@@ -1,8 +1,36 @@
-const express=require('express')
-const app=express()
+const express = require('express')
+const app = express()
 require('dotenv').config()
-const port=process.env.PORT
+const port = process.env.PORT
+const models = require('./models')
+const bodyParser = require('body-parser')
 
-app.listen(port,()=>{
+// server creation
+app.listen(port, () => {
     console.log(`server is running on ${port}`)
 })
+
+//use bodyparser
+app.use(bodyParser.json())
+
+//create one api post details 
+app.post('/v1/send-emails', (req, res) => {
+    const info = {
+        name: req.body.name,
+        email: req.body.email,
+        subject: req.body.subject,
+        message: req.body.message
+    }
+    models.contact.create(info).then(result => {
+        return res.status(201).json({
+            message: "we will contact you",
+            info: result
+        }).catch(error => {
+            return res.status(500).json({
+                message: "something went wrong",
+                error: error
+            })
+        })
+    })
+})
+
