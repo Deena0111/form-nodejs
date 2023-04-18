4805cf62c69575af1441f064941f8a0815bfb03b
Deena0111 <deena011197@gmail.com>
2023-04-18 13:23:24 +0530

    validation added

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ require('dotenv').config()
 const port = process.env.PORT
 const models = require('./models')
 const bodyParser = require('body-parser')
+const Validator = require("fastest-validator");
 
 // server creation
 app.listen(port, () => {
@@ -14,23 +15,43 @@ app.listen(port, () => {
 app.use(bodyParser.json())
 
 //create one api post details 
-app.post('/v1/send-emails', (req, res) => {
+app.post('/v1/send-emails', (req, res, next) => {
     const info = {
         name: req.body.name,
         email: req.body.email,
         subject: req.body.subject,
         message: req.body.message
     }
-    models.contact.create(info).then(result => {
-        return res.status(201).json({
-            message: "we will contact you",
-            info: result
-        }).catch(error => {
-            return res.status(500).json({
-                message: "something went wrong",
-                error: error
-            })
+    const schema = {
+        name: { type: "string", optional: false, min: "2", max: "30"},
+        email: { type: "email", optional: false },
+        subject: { type: "string", optional: false, min: "10", max: "200" },
+        message: { type: "string", optional: false, min: "20", max: "500" }
+    }
+    const v = new Validator();
+    const validationResponse = v.validate(info, schema);
+    if (validationResponse !== true) {
+        return res.status(400).json({
+            message: "Validation failed",
+            errors: validationResponse
         })
-    })
+    } else {
+        models.contact.create(info)
+            .then(result => {
+                res.json({
+                    statusCode: 201,
+                    message: "we will contact you",
+                    info: result
+                })
+            })
+            .catch(error => {
+                res.json({
+                    statusCode: 500,
+                    message: "something went wrong",
+                    error: error
+                })
+            });
+    }
 })
+
 
